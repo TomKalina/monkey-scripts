06834aad922cb8c05bc1108b2dfd0449d1d1610b
Tonda (Tomáš) Kalina <kalina@shoptet.cz>
2024-11-16 20:23:05 +0100

    Update excaliburshop user script to version 0.3, change API endpoints to Whiskybase, and enhance rating display functionality.

--- excaliburshop.user.js
+++ excaliburshop.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         excaliburshop script
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Rating of whisky
 // @author       Tomas Kalina
 // @match        https://www.excaliburshop.com/*
 // @require      file:///Users/tom/dev/monkey-scripts/excaliburshop.user.js
-// @connect      api.distilld.io
-// @connect      static.distilld.io
+// @connect      api.whiskybase.com
+// @connect      static.whiskybase.com
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
@@ -23,60 +23,48 @@
 
   for (let i = 0; i < items.length; i++) {
     const item = items[i];
-    const data = await loadRequest(item.innerText);
-    const rating = getRating(data);
+    const betterName = makeBetterName(item.innerText);
+    const data = await loadRequest(betterName);
+    const bottle = getBottleBOject(data);
 
-    if (rating) {
-      const ratingElement = document.createElement("div");
-      let html = "";
-      if (rating.rating > 0) {
-        html = `${rating.rating}: ${getStarRating(rating.rating)}`;
+    const ratingElement = document.createElement("div");
+    let html = "";
+    if (bottle) {
+      if (bottle.rating > 0) {
+        html = `${bottle.rating}`;
+        // html += `: ${getStarRating(bottle.rating)}`;
       }
 
-      if (rating.link) {
-        html += `<a href="https://distilld.io/whisky/${rating.link}" target="_blank">distilld.io</a>`;
-
-        const thumbnailLink = `https://static.distilld.io/thumbnail/${rating.thumbnail}.webp`;
-
-        // Načtení obrázku pomocí Base64
-        loadImage(thumbnailLink, ratingElement)
-      }
-
-      ratingElement.innerHTML = html;
-      item.parentNode.insertBefore(ratingElement, item.nextSibling);
+      html += `<a href="https://www.whiskybase.com/whiskies/whisky/${bottle.id}" target="_blank">detail</a>`;
+      loadImage(bottle.photo?.normal, ratingElement, data?.data[0]);
     }
+    html += ` <a href="https://www.whiskybase.com/search-v1/?q=${betterName}" target="_blank">search</a>`;
+    ratingElement.innerHTML = html;
+    item.parentNode.insertBefore(ratingElement, item.nextSibling);
   }
 })();
 
 /**
  * Extrahuje hodnocení ze získaných dat.
- * @param {*} data
- * @returns {{rating: string, link: string| undefined,thumbnail: string| undefined}| undefined} Hodnocení
+ * @param {HttpResponse} data
+ * @returns {DataView} Hodnocení
  */
-function getRating(data) {
-  const id = data?.hits?.hits[0]?._id;
-  const source = data?.hits?.hits[0]?._source;
-  const average_rating = Number(source?.average_rating);
-
-  if (source) {
-    return { rating: average_rating.toFixed(1), link: source?.name_key && `${source?.name_key}-${id}`, thumbnail: source?.thumbnail };
+function getBottleBOject(data) {
+  console.log(data);
+  if (data?.data.length === 0) {
+    return undefined;
   }
+  return data?.data[0];
 }
 
 /**
- * Převádí číselné hodnocení na hvězdičkové hodnocení.
- * @param {number} rating Číselné hodnocení.
- * @returns {string} Hodnocení jako hvězdičky.
+ *
+ * @param {string} thumbnailLink
+ * @param {string} ratingElement
+ * @param {string} name
+ * @param {WhiskyData} data
  */
-function getStarRating(rating) {
-  const fullStars = Math.floor(rating);
-  const halfStar = rating % 1 >= 0.5 ? 1 : 0;
-  const emptyStars = 5 - fullStars - halfStar;
-
-  return "★".repeat(fullStars) + (halfStar ? "½" : "") + "☆".repeat(emptyStars);
-}
-
-function loadImage(thumbnailLink, ratingElement) {
+function loadImage(thumbnailLink, ratingElement, data) {
   GM_xmlhttpRequest({
     method: "GET",
     url: thumbnailLink,
@@ -91,22 +79,32 @@ function loadImage(thumbnailLink, ratingElement) {
 
       const dataUrl = `data:image/webp;base64,${base64Image}`;
 
-      const thumbnailImage = document.createElement("img");
-      thumbnailImage.src = dataUrl;
-      thumbnailImage.style.display = "none";
-      thumbnailImage.style.position = "absolute";
-      thumbnailImage.style.zIndex = "1000";
-      document.body.appendChild(thumbnailImage);
+      const detailDiv = document.createElement("div");
+      detailDiv.style.display = "none";
+      detailDiv.style.position = "absolute";
+      detailDiv.style.zIndex = "1000";
+      detailDiv.style.backgroundColor = "white";
+      detailDiv.style.border = "1px solid black";
+
+      const title = document.createElement("div");
+      title.innerHTML = data?.name;
+      detailDiv.appendChild(title);
+
+      const image = document.createElement("img");
+      image.src = dataUrl;
+      detailDiv.appendChild(image);
+
+      document.body.appendChild(detailDiv);
 
       ratingElement.addEventListener("mouseover", () => {
-        thumbnailImage.style.display = "block";
+        detailDiv.style.display = "block";
         const rect = ratingElement.getBoundingClientRect();
-        thumbnailImage.style.top = `${rect.top + window.scrollY + 50}px`;
-        thumbnailImage.style.left = `${rect.right + window.scrollX - 200}px`;
+        detailDiv.style.top = `${rect.top + window.scrollY + 50}px`;
+        detailDiv.style.left = `${rect.right + window.scrollX - 200}px`;
       });
 
       ratingElement.addEventListener("mouseout", () => {
-        thumbnailImage.style.display = "none";
+        detailDiv.style.display = "none";
       });
     },
     onerror: function (error) {
@@ -116,27 +114,39 @@ function loadImage(thumbnailLink, ratingElement) {
 }
 
 /**
+ *
+ * @param {string} name
+ * @returns {string}
+ */
+function makeBetterName(name) {
+  const betterName = name
+    .split(" ")
+    .filter(
+      (word) => word.length >= 3 && !/\d/.test(word) && !/[áí]/i.test(word)
+    )
+    .join(" ");
+ 
+  console.log("name:", name, "betterName:", betterName);
+
+  return betterName;
+}
+
+/**
  * Načítá hodnocení whisky ze vzdáleného API.
  * @param {string} name Název whisky
- * @returns {Promise<Object>} Data z API
+ * @returns {Promise<HttpResponse>} Data z API
  */
 function loadRequest(name) {
   return new Promise((resolve, reject) => {
     const body = {
-      searchQuery: name,
-      tastingProfile: [],
-      whiskyType: [],
-      price: { values: ["0", "900+"] },
-      country: [],
-      region: [],
-      rating: "",
-      sort: "_score-desc",
+      query: name,
+      perpage: 20,
       page: 1,
     };
 
     GM_xmlhttpRequest({
-      method: "PUT",
-      url: "https://api.distilld.io/search/filtered",
+      method: "POST",
+      url: "https://api.whiskybase.com/api/v1/search",
       headers: {
         "Content-Type": "application/json",
       },
@@ -157,3 +167,65 @@ function loadRequest(name) {
     });
   });
 }
+
+/**
+ * HTTP Response
+ * @typedef {Object} HttpResponse
+ * @property {boolean} success - Indicates if the request was successful.
+ * @property {number} current_page - The current page number in pagination.
+ * @property {number} from - The starting item number of the current page.
+ * @property {number} to - The ending item number of the current page.
+ * @property {number} total - Total number of items available.
+ * @property {number} per_page - Number of items per page.
+ * @property {number} last_page - Total number of pages.
+ * @property {string} path - API endpoint for the search.
+ * @property {string} first_page_url - URL for the first page.
+ * @property {string} last_page_url - URL for the last page.
+ * @property {string|null} next_page_url - URL for the next page, or null if it doesn't exist.
+ * @property {string|null} prev_page_url - URL for the previous page, or null if it doesn't exist.
+ * @property {Array<WhiskyData>} data - List of whisky items on the current page.
+ * @property {Array<number>} years - List of years related to the items.
+ * @property {Array<number>} bottled - List of bottled years.
+ * @property {Array<string>} strength - List of strength percentages.
+ * @property {Array<string>} casknumbers - List of cask numbers.
+ */
+
+/**
+ * Whisky data
+ * @typedef {Object} WhiskyData
+ * @property {number} id - Unique ID of the whisky.
+ * @property {string} brandname - Brand name of the whisky.
+ * @property {string} name - Name of the whisky.
+ * @property {string|null} bottle_code - Bottle code, if available.
+ * @property {string} barcode - Barcode of the whisky.
+ * @property {string|null} cask_number - Cask number, if available.
+ * @property {number|null} vintage_year - Year of vintage, if available.
+ * @property {string} strength_unit - Unit of strength, typically "%vol".
+ * @property {string} strength - Strength of the whisky.
+ * @property {string} rating - Rating of the whisky.
+ * @property {number} bottle_date_year - Year the bottle was dated.
+ * @property {string|null} otherreleases - Other releases, if available.
+ * @property {string} bottler - Bottler information.
+ * @property {number} age - Age of the whisky.
+ * @property {string} whisky_type - Type of the whisky (e.g., Single Malt).
+ * @property {number|null} itemsforsale - Number of items for sale, if available.
+ * @property {number} bottle_size - Size of the bottle in milliliters.
+ * @property {string} country_code - Country code of the whisky's origin.
+ * @property {string} country_name - Country name of the whisky's origin.
+ * @property {string} region_name - Region name where the whisky is produced.
+ * @property {string} serie - Series name, if available.
+ * @property {string} bottle_for - Information about the intended market (e.g., "Travel Exclusive").
+ * @property {string} bottle_date - Date of the bottle.
+ * @property {string} suggest - Suggested name for the whisky.
+ * @property {string} suggest_brand - Suggested brand name.
+ * @property {Photo} photo - Photo URLs for the whisky.
+ */
+
+/**
+ * Photo data
+ * @typedef {Object} Photo
+ * @property {number} id - Photo ID.
+ * @property {string} small - URL for the small photo.
+ * @property {string} normal - URL for the normal photo.
+ * @property {string} big - URL for the big photo.
+ */
